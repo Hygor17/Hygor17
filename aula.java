57862569bfc8b2be27146b8f656729f9672845fd
Hygor17 <hygorfieszt4@gmail.com>
2026-03-16 21:06:39 -0300

    Create aula.java

---
+++ aula.java
@@ -0,0 +1,11 @@
+// Meu primeiro programa Java
+public class OlaMundo {
+
+    public static void main(String[] args) {
+
+        // Imprime uma mensagem no console
+        System.out.println("Olá, Mundo!");
+        System.out.println("Bem-vindo ao Java!");
+        System.out.println("Meu primeiro programa funciona!");
+    }
+}
